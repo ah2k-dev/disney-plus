2419d136e1fa1fe7b7c893dc58bb9fe92bf95cf6
ahmedhassan2k <ah2k.dev@gmail.com>
2021-11-20 05:23:51 +0500

    Added Header

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+function Home() {
+    return (
+        <Container>
+            3:10
+        </Container>
+    )
+}
+
+export default Home
+
+const Container = styled.main`
+    minn-height: calc(100vh - 70px);
+    padding: 0 calc(3.5vw - 5px);
+`
